db30d787dc573dac6b284ad7420296eccb95f08f
Chakipaki <belyaeve2@icloud.com>
2022-04-27 20:05:01 +0300

    Add logic for searchbar

--- src/app/header/components/navbar/components/searchbar/searchbar.component.ts
+++ src/app/header/components/navbar/components/searchbar/searchbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-searchbar',
@@ -14,7 +14,12 @@ export class SearchbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit(form: any): void {
+  onSubmit(form: any, field: any): void {
     this.status = false
+    console.log(field)
+  }
+
+  clearSearchField(field: any):void {
+    field.reset()
   }
 }
