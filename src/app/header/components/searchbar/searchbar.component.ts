9edd78da26afaefdf7d78ea2f1f68dae46bf4931
Chakipaki <belyaeve2@icloud.com>
2022-04-29 09:27:31 +0300

    Change status

--- src/app/header/components/searchbar/searchbar.component.ts
+++ src/app/header/components/searchbar/searchbar.component.ts
@@ -1,22 +1,38 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, Input, NgZone, OnInit, ViewChild} from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {Form} from "@angular/forms";
 
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
-  styleUrls: ['./searchbar.component.scss']
+  styleUrls: ['./searchbar.component.scss'],
+  animations: [
+    trigger('hideSearch', [
+      state('start', style({
+        width: '850px'
+      })),
+      state('end', style({
+        width: '0'
+      })),
+      transition('start => end', [
+        animate('5s')
+      ]),
+    ],),
+  ]
 })
 export class SearchbarComponent implements OnInit {
 
   @Input() status: boolean = false
+  animationStatus = 'end'
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
-  onSubmit(form: any, field: any): void {
+  onSubmit(form: Form): void {
     this.status = false
-    console.log(field)
   }
 
   clearSearchField(field: any):void {
